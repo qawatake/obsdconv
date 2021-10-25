229e8765acf69dde3fae286c98c5e07b60f9f6be
qawatake <greenmackey@gmail.com>
2021-10-25 10:55:19 +0000

    [NEW] git add walk.go

---
+++ walk.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+func walk(src string, dst string) error {
+	err := filepath.Walk(src, func(path string, info fs.FileInfo, err error) error {
+		rpath, err := filepath.Rel(src, path)
+		if err != nil {
+			return err
+		}
+		rpath = filepath.Clean(rpath)
+		newpath := dst + "/" + rpath
+		if info.IsDir() {
+			if _, err := os.Stat(newpath); !os.IsNotExist(err) {
+				return nil
+			}
+			if err := os.Mkdir(newpath, 0o777); err == nil {
+				return nil
+			} else {
+				return err
+			}
+		}
+		if filepath.Ext(path) != ".md" {
+			file, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+			newfile, err := os.Create(newpath)
+			if err != nil {
+				return err
+			}
+			defer newfile.Close()
+			io.Copy(newfile, file)
+			fmt.Printf("md file: %s -> %s\n", path, dst+"/"+rpath)
+		} else {
+			fmt.Printf("other file: %s -> %s\n", path, dst+"/"+rpath)
+		}
+		return nil
+	})
+	return err
+}
