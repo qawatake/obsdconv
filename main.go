1070593aeb372e62b31f67639387cd320a7d8153
qawatake <greenmackey@gmail.com>
2021-10-27 12:53:21 +0900

    [REFACTORING] move functions from main.go

--- main.go
+++ main.go
@@ -4,10 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
-
-	"github.com/qawatake/obsd2hugo/convert"
-	"gopkg.in/yaml.v2"
 )
 
 const (
@@ -55,93 +51,4 @@ func main() {
 	if err := walk(&flags); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func process(vault string, newpath string, flags *flagBundle, file *os.File) error {
-	fileinfo, err := file.Stat()
-	if err != nil {
-		return fmt.Errorf("file.Stat failed %w", err)
-	}
-
-	content := make([]byte, fileinfo.Size())
-	_, err = file.Read(content)
-	if err != nil {
-		return fmt.Errorf("file.Write failed: %w", err)
-	}
-
-	yml, body := splitMarkdown([]rune(string(content)))
-	frontmatter := new(frontMatter)
-	title := ""
-	tags := make(map[string]struct{})
-
-	newContent, err := converts(body, vault, &title, tags, *flags)
-	if err != nil {
-		return fmt.Errorf("convert failed: %w", err)
-	}
-
-	if err := yaml.Unmarshal(yml, frontmatter); err != nil {
-		return fmt.Errorf("yaml.Unmarshal failed: %w", err)
-	}
-	if flags.title {
-		frontmatter.Title = title
-	}
-	if flags.alias {
-		frontmatter.Aliases = append(frontmatter.Aliases, frontmatter.Title)
-	}
-	if flags.cptag {
-		for key := range tags {
-			frontmatter.Tags = append(frontmatter.Tags, key)
-		}
-	}
-	yml, err = yaml.Marshal(frontmatter)
-	if err != nil {
-		return fmt.Errorf("yaml.Marshal failed: %w", err)
-	}
-
-	newfile, err := os.Create(newpath)
-	if err != nil {
-		return fmt.Errorf("failed to create %s: %w", newpath, err)
-	}
-	defer newfile.Close()
-	fmt.Fprintf(newfile, "---\n%s---\n%s", string(yml), string(newContent))
-	return nil
-}
-
-func converts(raw []rune, vault string, title *string, tags map[string]struct{}, flags flagBundle) (output []rune, err error) {
-	output = raw
-	if flags.cptag {
-		_, err = convert.NewTagFinder(tags).Convert(output)
-		if err != nil {
-			return nil, fmt.Errorf("TagFinder failed: %w", err)
-		}
-	}
-	if flags.rmtag {
-		output, err = convert.NewTagRemover().Convert(output)
-		if err != nil {
-			return nil, fmt.Errorf("TagRemover failed: %w", err)
-		}
-	}
-	if flags.cmmt {
-		output, err = convert.NewCommentEraser().Convert(output)
-		if err != nil {
-			return nil, fmt.Errorf("CommentEraser failed: %w", err)
-		}
-	}
-	if flags.link {
-		output, err = convert.NewLinkConverter(vault).Convert(output)
-		if err != nil {
-			return nil, fmt.Errorf("LinkConverter failed: %w", err)
-		}
-	}
-	if flags.title {
-		titleFoundFrom, _ := convert.NewTagRemover().Convert(output)
-		if err != nil {
-			return nil, fmt.Errorf("preprocess TagRemover for finding titles failed: %w", err)
-		}
-		_, err = convert.NewTitleFinder(title).Convert(titleFoundFrom)
-		if err != nil {
-			return nil, fmt.Errorf("TitleFinder failed: %w", err)
-		}
-	}
-	return output, nil
 }
--- walk.go
+++ walk.go
@@ -9,6 +9,7 @@ import (
 	"path/filepath"
 
 	"github.com/qawatake/obsd2hugo/convert"
+	"gopkg.in/yaml.v2"
 )
 
 func walk(flags *flagBundle) error {
@@ -47,13 +48,69 @@ func walk(flags *flagBundle) error {
 				return err
 			}
 			defer file.Close()
-			if err := process(flags.src, newpath, flags, file); err != nil {
+			if err := process(flags.src, path, newpath, flags); err != nil {
 				return handleErr(path, err)
 			}
 		}
 		return nil
 	})
 	return err
+}
+
+func process(vault string, path string, newpath string, flags *flagBundle) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("failed to open %s", path)
+	}
+	defer file.Close()
+
+	fileinfo, err := file.Stat()
+	if err != nil {
+		return fmt.Errorf("file.Stat failed %w", err)
+	}
+
+	content := make([]byte, fileinfo.Size())
+	_, err = file.Read(content)
+	if err != nil {
+		return fmt.Errorf("file.Write failed: %w", err)
+	}
+
+	yml, body := splitMarkdown([]rune(string(content)))
+	frontmatter := new(frontMatter)
+	title := ""
+	tags := make(map[string]struct{})
+
+	newContent, err := converts(body, vault, &title, tags, *flags)
+	if err != nil {
+		return fmt.Errorf("convert failed: %w", err)
+	}
+
+	if err := yaml.Unmarshal(yml, frontmatter); err != nil {
+		return fmt.Errorf("yaml.Unmarshal failed: %w", err)
+	}
+	if flags.title {
+		frontmatter.Title = title
+	}
+	if flags.alias {
+		frontmatter.Aliases = append(frontmatter.Aliases, frontmatter.Title)
+	}
+	if flags.cptag {
+		for key := range tags {
+			frontmatter.Tags = append(frontmatter.Tags, key)
+		}
+	}
+	yml, err = yaml.Marshal(frontmatter)
+	if err != nil {
+		return fmt.Errorf("yaml.Marshal failed: %w", err)
+	}
+
+	newfile, err := os.Create(newpath)
+	if err != nil {
+		return fmt.Errorf("failed to create %s: %w", newpath, err)
+	}
+	defer newfile.Close()
+	fmt.Fprintf(newfile, "---\n%s---\n%s", string(yml), string(newContent))
+	return nil
 }
 
 func handleErr(path string, err error) error {
