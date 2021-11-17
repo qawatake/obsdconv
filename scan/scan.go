a6098cb77a49ccaccfe7329f0334fb34827eb9da
qawatake <greenmackey@gmail.com>
2021-11-17 17:23:26 +0900

    [REFACTORING] scan: move funcs to util.go

--- scan/scan.go
+++ scan/scan.go
@@ -5,38 +5,6 @@ import (
 	"unicode"
 )
 
-func unescaped(raw []rune, ptr int, substr string) bool {
-	length := len([]rune(substr))
-	if len(raw[ptr:]) < length {
-		return false
-	}
-	if string(raw[ptr:ptr+length]) != substr {
-		return false
-	}
-	if ptr > 0 && unescaped(raw, ptr-1, "\\") {
-		return false
-	}
-	return true
-}
-
-func precededBy(raw []rune, ptr int, ss []string) bool {
-	for _, substr := range ss {
-		if ptr >= len(substr) && string(raw[ptr-len([]rune(substr)):ptr]) == substr {
-			return true
-		}
-	}
-	return false
-}
-
-func followedBy(raw []rune, ptr int, ss []string) bool {
-	for _, substr := range ss {
-		if len(raw[ptr+1:]) >= len(substr) && string(raw[ptr+1:ptr+1+len([]rune(substr))]) == substr {
-			return true
-		}
-	}
-	return false
-}
-
 func ScanInlineCode(raw []rune, ptr int) (advance int) {
 	if !(unescaped(raw, ptr, "`") && len(raw[ptr:]) > 1) {
 		return 0
