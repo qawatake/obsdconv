830ad83fb71654b73e96831bde0aacec1ee5d1dc
qawatake <greenmackey@gmail.com>
2021-11-21 22:54:08 +0900

    [NEW] main: use cmdflags in TestRun

--- run_test.go
+++ run_test.go
@@ -2,32 +2,55 @@ package main
 
 import (
 	"bytes"
+	"flag"
+	"fmt"
+	"path/filepath"
 	"testing"
 )
 
 func TestRun(t *testing.T) {
-	// rootDir := "sample"
-	// gotDstDir := filepath.Join(rootDir, "tmp")
+	rootDir := "sample"
+	src := "src"
+	dst := "dst"
+	tmp := "tmp"
 	cases := []struct {
 		name        string
-		flags       flagBundle
+		cmdflags    map[string]string
 		argVersion  string
 		wantDstDir  string
 		wantVersion string
 	}{
 		{
 			name: "-version",
-			flags: flagBundle{
-				ver: true,
+			cmdflags: map[string]string{
+				FLAG_VERSION: "1",
 			},
 			argVersion:  "1.0.0",
 			wantVersion: "v1.0.0\n",
 		},
+		{
+			name: "-obs",
+			cmdflags: map[string]string{
+				FLAG_SOURCE:         filepath.Join(rootDir, "obs", src),
+				FLAG_DESTINATION:    filepath.Join(rootDir, "obs", tmp),
+				FLAG_OBSIDIAN_USAGE: "1",
+			},
+			wantDstDir: filepath.Join(rootDir, "obs", dst),
+		},
 	}
 
 	for _, tt := range cases {
+		// flags を設定
+		flags := new(flagBundle)
+		flagset := flag.NewFlagSet(fmt.Sprintf("TestSetFlags | %s", tt.name), flag.ExitOnError)
+		initFlags(flagset, flags)
+		for cmdname, cmdvalue := range tt.cmdflags { // flag.Parse() に相当
+			flagset.Set(cmdname, cmdvalue)
+		}
+		setFlags(flagset, flags)
+
 		versionBuf := new(bytes.Buffer)
-		err := run(tt.argVersion, &tt.flags, versionBuf)
+		err := run(tt.argVersion, flags, versionBuf)
 		if err != nil {
 			t.Fatalf("[FATAL | %s] unexpected err occurred: %v", tt.name, err)
 		}
@@ -38,7 +61,7 @@ func TestRun(t *testing.T) {
 			continue
 		}
 
-		if !equalDirContent(tt.flags.dst, tt.wantDstDir) {
+		if !equalDirContent(flags.dst, tt.wantDstDir) {
 			t.Fatalf("[ERROR | content - %s]", tt.name)
 		}
 	}
