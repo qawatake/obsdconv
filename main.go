49b81bdfc89c9026b5a5a0977c239819e6c30868
qawatake <greenmackey@gmail.com>
2021-11-21 22:28:19 +0900

    [NEW] main: add TestRun

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"os"
 	"path/filepath"
 
 	"github.com/qawatake/obsdconv/process"
@@ -18,33 +20,34 @@ const (
 )
 
 func main() {
-	version, err := run(flag.CommandLine)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if version != "" {
-		fmt.Println(version)
-	}
-}
-
-func run(flagset *flag.FlagSet) (version string, err error) {
+	// flags を設定
 	flags := new(flagBundle)
 	initFlags(flag.CommandLine, flags)
 	flag.Parse()
-	if flags.ver {
-		return fmt.Sprintf("v%s\n", Version), nil
-	}
 	setFlags(flag.CommandLine, flags)
+
+	// main 部分
+	err := run(Version, flags, os.Stdout)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(version string, flags *flagBundle, w io.Writer) (err error) {
+	if flags.ver {
+		fmt.Fprintf(w, "v%s\n", version)
+		return nil
+	}
 	if err := verifyFlags(flags); err != nil {
-		return "", err
+		return err
 	}
 	processor := newDefaultProcessor(flags)
 	skipper, err := process.NewSkipper(filepath.Join(flags.src, DEFAULT_IGNORE_FILE_NAME))
 	if err != nil {
-		return "", err
+		return err
 	}
 	if err := process.Walk(flags.src, flags.dst, skipper, processor); err != nil {
-		return "", err
+		return err
 	}
-	return "", nil
+	return nil
 }
